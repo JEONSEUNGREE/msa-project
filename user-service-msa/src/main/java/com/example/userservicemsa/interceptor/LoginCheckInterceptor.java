7b54de4eff5bcf50bfd4b433da9b607ba424b43a
전승리 <jws08219@naver.com>
2023-01-16 23:59:17 +0900

    login_check annotation

---
+++ user-service-msa/src/main/java/com/example/userservicemsa/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,39 @@
+package com.example.userservicemsa.interceptor;
+
+import com.example.userservicemsa.exception.UserNotFoundException;
+import com.example.userservicemsa.interceptor.annotation.LoginCheck;
+import com.example.userservicemsa.user.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.handler.HandlerMethod;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler)
+            throws Exception {
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        LoginCheck loginCheck = handlerMethod.getMethodAnnotation(LoginCheck.class);
+
+        if (loginCheck == null) {
+            return true;
+        }
+
+        if (SecurityContextHolder.getContext() == null) {
+            throw new UserNotFoundException("LOGIN PLEASE");
+        }
+
+        return true;
+
+    }
+}
